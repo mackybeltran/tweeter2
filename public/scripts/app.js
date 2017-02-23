a281fa8ceaa529b082486f4873b77bd3ab664da5
macky <mackybeltran1@gmail.com>
2017-02-23 18:01:19 +0000

    adding dynamic tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,89 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function($){
+function createTweetElement (tweetObj){
+  let output = ""
+  output += `<article>
+          <header>
+              <img class="avatar" src="${tweetObj.user.avatars.small}" width="40px" height="40px">
+              <h2 class="username">${tweetObj.user.name}</h2>
+              <span class="tweeter-handle">${tweetObj.user.handle}</span>
+          </header>
+          <p class="tweettext">${tweetObj.content.text}</p>
+          <footer>
 
+              ${tweetObj.created_at}
+            <div class="icons">
+              <i class="material-icons">favorite</i>
+              <i class="material-icons">cached</i>
+              <i class="material-icons">bookmark</i>
+            </div>
+
+
+          </footer>`;
+          return output
+}
+
+// Fake data taken from tweets.json
+var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+
+
+function renderTweets(tweets){
+  var $tweets = $("#old-tweets");
+  tweets.forEach(function(tweet) {
+    $($tweets).prepend(createTweetElement(tweet))
+    console.log(tweets)
+  });
+}
+renderTweets(data)
+
+
+
+})
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,12 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
-    $( "textarea" ).keypress(function() {
-  console.log(this);
-  });
+$(document).ready(function($){
+ $("textarea").keypress(updateCountdown);
 });
+//Function for countdown
+function updateCountdown () {
+ var remaining = 140 - $(this).val().length;
+ $(this).siblings(".counter").text(remaining);
+ if (remaining < 0) {
+   $(".counter").css("color", "red");
+ }
+};
