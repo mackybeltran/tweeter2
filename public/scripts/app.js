575fc4f1984c8c2be49f7f8b5df3ee6963a348c6
macky <mackybeltran1@gmail.com>
2017-02-24 17:21:40 +0000

    adding compose button css

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,55 +26,9 @@ function createTweetElement (tweetObj){
           </footer>`;
           return output
 }
-
-// Fake data taken from tweets.json
-// var data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//     },
-//     "created_at": 1461113796368
-//   }
-// ];
-
+$(".compose").click(function(){
+  $(".tweetText").focus()
+})
 
 
 function renderTweets(tweets){
@@ -86,19 +40,23 @@ function renderTweets(tweets){
 
 function loadNewTweet(){
   const formData = $("textarea").val()
+    if (!formData.length){
+      alert("Please Type Text to Tweet!");
+      return;
+    } if (formData.length > 140){
+      alert("Max 140 Characters. Try Again.")
+    } else {
   $.ajax({
     method: 'POST',
     url: ("/tweets"),
     data: {text: formData}
 
 }).then(loadTweetData)
-  .fail(console.error)
-
+}
 
 }
 
 $(".tweetbtn").click(function(event){
-  console.log('Button clicked, performing ajax call...');
   event.preventDefault();
   loadNewTweet();
   $("textarea").val("");
@@ -116,9 +74,7 @@ function loadTweetData() {
 // renderTweets(data)
 loadTweetData();
 
-//click listener for "tweetbtn"
-
-
 
 
 })
+
